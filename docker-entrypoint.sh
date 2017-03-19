73fbe6431e5745d87d68a744a2567a5b15c5e76a
foxboxsnet <naokiaoyama2009@gmail.com>
2017-03-19 17:19:28 +0900

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 #// config
 #/////////////////////////////////////////////////
 
-sed -ie "s/RANCHER_SERVER/"${RANCHER_SERVER}"/" /etc/nginx/conf.d/rancher-server_proxy.conf
+sed -ie "s/RANCHER_SERVER/"${RANCHER_SERVER_IP}"/" /etc/nginx/conf.d/rancher-server_proxy.conf
 
 #/////////////////////////////////////////////////
 #// nginx exec
